5a3f7bd525b355a4840e20644d90624b74703fc1
Ma Keng Chon <dwkeis@gmail.com>
2020-12-07 16:12:02 +0800

    feat(trying quasar): its a very first test

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="q-pa-md">
+    <q-layout view="hHh Lpr lff">
+      <q-header elevated>
+        <q-toolbar>
+          <q-btn
+            flat
+            @click="drawerLeft = !drawerLeft"
+            round
+            dense
+            icon="menu"
+          />
+          <q-toolbar-title>
+            KC's first test
+          </q-toolbar-title>
+
+          <div>Quasar v{{ $q.version }}</div>
+        </q-toolbar>
+      </q-header>
+
+      <q-drawer
+        v-model="drawerLeft"
+        show-if-above
+        :breakpoint="700"
+        :width="310"
+        elevated
+      >
+        <q-scroll-area class="fit">
+          <div class="q-pa-sm">
+            <q-date v-model="date" />
+          </div>
+        </q-scroll-area>
+      </q-drawer>
+
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  data() {
+    return {
+      drawerLeft: false,
+      date: "2020/12/07"
+    };
+  }
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <div style="max-width: 800px; width: 100%;">
+      <q-calendar
+        v-model="selectedDate"
+        view="3day"
+        locale="en-us"
+        style="height: 400px;"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "Dayview",
+  data() {
+    return {
+      selectedDate: ""
+    };
+  }
+};
+</script>
