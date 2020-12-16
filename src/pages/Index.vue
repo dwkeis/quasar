9f647fec0dac99886a72b546b9e565f4c16a6493
Ma Keng Chon <dwkeis@gmail.com>
2020-12-16 18:45:49 +0800

    feat: can add event

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -24,12 +24,12 @@
                 autofocus
               >
                 <template v-slot:before>
-                  <q-icon name="title" />
+                  <q-icon name="eva-text" />
                 </template>
               </q-input>
               <q-input v-model="eventForm.details" placeholder="Details">
                 <template v-slot:before>
-                  <q-icon name="subject" />
+                  <q-icon name="eva-list" />
                 </template>
               </q-input>
             </q-card-section>
@@ -43,7 +43,7 @@
                 mask="####-##-## ##:##"
               >
                 <template v-slot:before>
-                  <q-icon name="add_task" />
+                  <q-icon name="eva-checkmark-circle" />
                 </template>
                 <template v-slot:append>
                   <q-btn flat round icon="event" class="cursor-pointer">
@@ -89,6 +89,7 @@
                   </q-btn>
                 </template>
               </q-input>
+              <br />
               <!--End Time-->
               <q-input
                 v-model="eventForm.dateTimeEnd"
@@ -144,6 +145,7 @@
                   </q-btn>
                 </template>
               </q-input>
+
               <!--Check All Day box-->
               <q-field v-model="eventForm.allDay" style="padding-bottom: 20px;">
                 <template v-slot:before>
@@ -155,7 +157,12 @@
 
             <q-card-actions align="right">
               <q-btn flat type="reset" label="Cancel" v-close-popup></q-btn>
-              <q-btn color="primary" type="submit" label="OK"></q-btn>
+              <q-btn
+                color="primary"
+                type="submit"
+                label="OK"
+                style="width:70px"
+              ></q-btn>
             </q-card-actions>
           </q-card>
         </q-form>
@@ -172,12 +179,12 @@
       v-model="selectedDate"
       view="week"
       locale="en-us"
-      style="height: 400px;"
       class="calendar-container"
       animated
       transition-prev="slide-right"
       transition-next="slide-left"
       @click:time2="addEventMenu"
+      :interval-height="50"
     >
       <!-- eslint-disable vue/no-unused-vars -->
       <template #day-body="{ timestamp, timeStartPos, timeDurationHeight }">
@@ -196,30 +203,12 @@
         </template>
       </template>
     </q-calendar>
-
-    <q-card
-      class="events col-4 q-pa-xs full-height column justify-start items-start"
-    >
-      <q-item-section class="full-width">
-        <q-item-label>Events</q-item-label>
-      </q-item-section>
-      <q-separator />
-      <div class="scroll overflow-auto" style="height: 360px; width: 100%;">
-        <div
-          v-for="(event, index) in events"
-          :key="index"
-          class="col-12"
-          style="font-size: 10px; line-height: 10px; max-height: 14px; min-height: 14px; padding: 2px 2px; white-space: nowrap;"
-        >
-          {{ event }}
-        </div>
-      </div>
-    </q-card>
   </q-page>
 </template>
 
 <script>
 import QCalendar from "@quasar/quasar-ui-qcalendar";
+import { date } from "quasar";
 
 const formDefault = {
   title: "",
@@ -227,7 +216,7 @@ const formDefault = {
   allDay: false,
   dateTimeStart: "",
   dateTimeEnd: "",
-  bgcolor: "#0000FF"
+  bgcolor: "#4fc3f7"
 };
 
 export default {
@@ -250,7 +239,7 @@ export default {
           date: "2020-12-15",
           time: "10:00",
           duration: 120,
-          bgcolor: "red",
+          bgcolor: "#4fc3f7",
           icon: "fas fa-handshake"
         },
         {
@@ -301,6 +290,14 @@ export default {
     onSubmit() {
       this.saveEvent();
     },
+    getDuration(dateTimeStart, dateTimeEnd, unit) {
+      const start = QCalendar.makeDateTime(
+        QCalendar.parseTimestamp(dateTimeStart)
+      );
+      const end = QCalendar.makeDateTime(QCalendar.parseTimestamp(dateTimeEnd));
+      const diff = date.getDateDiff(end, start, unit);
+      return diff;
+    },
     saveEvent() {
       const self = this;
       this.$refs.event.validate().then(success => {
@@ -338,10 +335,8 @@ export default {
               self.events.splice(index, 1, { ...data });
             }
           } else {
-            // add to events array
             self.events.push(data);
           }
-
           self.contextDay = null;
         }
       });
@@ -365,13 +360,77 @@ export default {
       const s = {};
       s["background-color"] = event.bgcolor;
       if (timeStartPos) {
-        s.top = timeStartPos(event.time) + "px";
+        s.top = timeStartPos(event.time, false) + "px";
+        s.position = "absolute";
+        if (event.side !== undefined) {
+          s.width = "50%";
+          if (event.side === "right") {
+            s.left = "50%";
+          }
+        } else {
+          s.width = "100%";
+        }
       }
       if (timeDurationHeight) {
         s.height = timeDurationHeight(event.duration) + "px";
       }
       s["align-items"] = "flex-start";
       return s;
+    },
+    checkDateTimeStart(/* val */) {
+      this.$refs.dateTimeEnd.resetValidation();
+      if (this.eventForm.dateTimeStart && this.eventForm.dateTimeEnd) {
+        const timestampStart = QCalendar.parseTimestamp(
+          this.eventForm.dateTimeStart
+        );
+        const timestampEnd = QCalendar.parseTimestamp(
+          this.eventForm.dateTimeEnd
+        );
+        const dayStart = QCalendar.getDayIdentifier(timestampStart);
+        const dayEnd = QCalendar.getDayIdentifier(timestampEnd);
+        if (dayStart < dayEnd) {
+          return true;
+        } else if (dayStart > dayEnd) {
+          return false;
+        } else {
+          const timeStart = QCalendar.getTimeIdentifier(timestampStart);
+          const timeEnd = QCalendar.getTimeIdentifier(timestampEnd);
+          if (timeStart <= timeEnd) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+      return false;
+    },
+
+    checkDateTimeEnd(val) {
+      this.$refs.dateTimeStart.resetValidation();
+      if (this.eventForm.dateTimeStart && this.eventForm.dateTimeEnd) {
+        const timestampEnd = QCalendar.parseTimestamp(
+          this.eventForm.dateTimeEnd
+        );
+        const timestampStart = QCalendar.parseTimestamp(
+          this.eventForm.dateTimeStart
+        );
+        const dayEnd = QCalendar.getDayIdentifier(timestampEnd);
+        const dayStart = QCalendar.getDayIdentifier(timestampStart);
+        if (dayEnd > dayStart) {
+          return true;
+        } else if (dayEnd < dayStart) {
+          return false;
+        } else {
+          const timeEnd = QCalendar.getTimeIdentifier(timestampEnd);
+          const timeStart = QCalendar.getTimeIdentifier(timestampStart);
+          if (timeEnd >= timeStart) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+      return false;
     },
     getEvents(dt) {
       const currentDate = QCalendar.parseTimestamp(dt);
